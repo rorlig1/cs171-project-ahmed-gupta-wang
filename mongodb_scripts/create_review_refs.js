8eb20f8f6a1d7e5d49d58582622dfbf924e0dfbe
Gaurav Gupta <ggupta@fas.harvard.edu>
2014-04-27 00:11:40 -0400

    revised data

--- mongodb_scripts/create_review_refs.js
+++ mongodb_scripts/create_review_refs.js
@@ -11,13 +11,17 @@ function getReviews (r) {
 	var reviewArr = []
 	db.review.find({business_id: r.business_id}).forEach(function(reviews) {
 
-		reviewArr.push(reviews.votes);
+		var reviewObj = {};
+		reviewObj["stars"] = reviews.stars;
+		reviewObj["votes"] = reviews.votes;
+
+		reviewArr.push(reviewObj);
 
 		print("reviews votes for business " + r.business_id + " review " + JSON.stringify(reviews.votes));
 
 	})	
 
-	db.business2.update({"_id": r._id}, {$set: {"votes": reviewArr}})
+	db.business2.update({"_id": r._id}, {$set: {"reviews": reviewArr}})
 
 }
 db.business2.find().forEach(getReviews)
