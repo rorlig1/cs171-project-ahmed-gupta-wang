652a2d9dc4ba6984ff83fde5ad96b17e930dc35a
Noorain A <noorain@gmail.com>
2014-04-28 00:01:20 -0700

    Adding New files with layout integrated.

---
+++ js/heatmap.js
@@ -0,0 +1,968 @@
+ combinations = [
+{
+ "lookup" : "0-0",
+"hour" : 0,
+"day": 0
+},
+{
+"lookup" : "1-0",
+"hour" : 1,
+"day": 0 
+},
+{
+ "lookup" : "2-0",
+"hour" : 2,
+"day": 0
+},
+{
+"lookup" : "3-0",
+"hour" : 3,
+"day": 0 
+},
+{
+ "lookup" : "4-0",
+"hour" : 4,
+"day": 0
+},
+{
+"lookup" : "5-0",
+"hour" : 5,
+"day": 0 
+},
+{
+ "lookup" : "6-0",
+"hour" : 6,
+"day": 0
+},
+{
+"lookup" : "7-0",
+"hour" : 7,
+"day": 0 
+},
+{
+"lookup" : "8-0",
+"hour" : 8,
+"day": 0 
+},
+{
+"lookup" : "9-0",
+"hour" : 9,
+"day": 0 
+},
+{
+"lookup" : "10-0",
+"hour" : 10,
+"day": 0 
+},
+{
+"lookup" : "11-0",
+"hour" : 11,
+"day": 0 
+},
+{
+"lookup" : "12-0",
+"hour" : 12,
+"day": 0 
+},
+{
+"lookup" : "13-0",
+"hour" : 13,
+"day": 0 
+},
+{
+"lookup" : "14-0",
+"hour" : 14,
+"day": 0 
+},
+{
+"lookup" : "15-0",
+"hour" : 15,
+"day": 0 
+},
+{
+"lookup" : "16-0",
+"hour" : 16,
+"day": 0 
+},
+{
+"lookup" : "17-0",
+"hour" : 17,
+"day": 0 
+},
+{
+"lookup" : "18-0",
+"hour" : 18,
+"day": 0 
+},
+{
+"lookup" : "19-0",
+"hour" : 19,
+"day": 0 
+},
+{
+"lookup" : "20-0",
+"hour" : 20,
+"day": 0 
+},
+{
+"lookup" : "21-0",
+"hour" : 21,
+"day": 0 
+},
+{
+"lookup" : "22-0",
+"hour" : 22,
+"day": 0 
+},
+{
+"lookup" : "23-0",
+"hour" : 23,
+"day": 0 
+},
+{
+ "lookup" : "0-1",
+"hour" : 0,
+"day": 1
+},
+{
+"lookup" : "1-1",
+"hour" : 1,
+"day": 1 
+},
+{
+ "lookup" : "2-1",
+"hour" : 2,
+"day": 1
+},
+{
+"lookup" : "3-1",
+"hour" : 3,
+"day": 1 
+},
+{
+ "lookup" : "4-1",
+"hour" : 4,
+"day": 1
+},
+{
+"lookup" : "5-1",
+"hour" : 5,
+"day": 1 
+},
+{
+ "lookup" : "6-1",
+"hour" : 6,
+"day": 1
+},
+{
+"lookup" : "7-1",
+"hour" : 7,
+"day": 1 
+},
+{
+"lookup" : "8-1",
+"hour" : 8,
+"day": 1 
+},
+{
+"lookup" : "9-1",
+"hour" : 9,
+"day": 1 
+},
+{
+"lookup" : "10-1",
+"hour" : 10,
+"day": 1 
+},
+{
+"lookup" : "11-1",
+"hour" : 11,
+"day": 1 
+},
+{
+"lookup" : "12-1",
+"hour" : 12,
+"day": 1 
+},
+{
+"lookup" : "13-1",
+"hour" : 13,
+"day": 1 
+},
+{
+"lookup" : "14-1",
+"hour" : 14,
+"day": 1 
+},
+{
+"lookup" : "15-1",
+"hour" : 15,
+"day": 1 
+},
+{
+"lookup" : "16-1",
+"hour" : 16,
+"day": 1 
+},
+{
+"lookup" : "17-1",
+"hour" : 17,
+"day": 1 
+},
+{
+"lookup" : "18-1",
+"hour" : 18,
+"day": 1 
+},
+{
+"lookup" : "19-1",
+"hour" : 19,
+"day": 1 
+},
+{
+"lookup" : "20-1",
+"hour" : 20,
+"day": 1 
+},
+{
+"lookup" : "21-1",
+"hour" : 21,
+"day": 1 
+},
+{
+"lookup" : "22-1",
+"hour" : 22,
+"day": 1 
+},
+{
+"lookup" : "23-1",
+"hour" : 23,
+"day": 1 
+},{
+ "lookup" : "0-2",
+"hour" : 0,
+"day": 2
+},
+{
+"lookup" : "1-2",
+"hour" : 1,
+"day": 2 
+},
+{
+ "lookup" : "2-2",
+"hour" : 2,
+"day": 2
+},
+{
+"lookup" : "3-2",
+"hour" : 3,
+"day": 2 
+},
+{
+ "lookup" : "4-2",
+"hour" : 4,
+"day": 2
+},
+{
+"lookup" : "5-2",
+"hour" : 5,
+"day": 2 
+},
+{
+ "lookup" : "6-2",
+"hour" : 6,
+"day": 2
+},
+{
+"lookup" : "7-2",
+"hour" : 7,
+"day": 2 
+},
+{
+"lookup" : "8-2",
+"hour" : 8,
+"day": 2 
+},
+{
+"lookup" : "9-2",
+"hour" : 9,
+"day": 2 
+},
+{
+"lookup" : "10-2",
+"hour" : 10,
+"day": 2 
+},
+{
+"lookup" : "11-2",
+"hour" : 11,
+"day": 2 
+},
+{
+"lookup" : "12-2",
+"hour" : 12,
+"day": 2 
+},
+{
+"lookup" : "13-2",
+"hour" : 13,
+"day": 2 
+},
+{
+"lookup" : "14-2",
+"hour" : 14,
+"day": 2 
+},
+{
+"lookup" : "15-2",
+"hour" : 15,
+"day": 2 
+},
+{
+"lookup" : "16-2",
+"hour" : 16,
+"day": 2 
+},
+{
+"lookup" : "17-2",
+"hour" : 17,
+"day": 2 
+},
+{
+"lookup" : "18-2",
+"hour" : 18,
+"day": 2 
+},
+{
+"lookup" : "19-2",
+"hour" : 19,
+"day": 2 
+},
+{
+"lookup" : "20-2",
+"hour" : 20,
+"day": 2 
+},
+{
+"lookup" : "21-2",
+"hour" : 21,
+"day": 2 
+},
+{
+"lookup" : "22-2",
+"hour" : 22,
+"day": 2 
+},
+{
+"lookup" : "23-2",
+"hour" : 23,
+"day": 2 
+},{
+ "lookup" : "0-3",
+"hour" : 0,
+"day": 3
+},
+{
+"lookup" : "1-3",
+"hour" : 1,
+"day": 3 
+},
+{
+ "lookup" : "2-3",
+"hour" : 2,
+"day": 3
+},
+{
+"lookup" : "3-3",
+"hour" : 3,
+"day": 3 
+},
+{
+ "lookup" : "4-3",
+"hour" : 4,
+"day": 3
+},
+{
+"lookup" : "5-3",
+"hour" : 5,
+"day": 3 
+},
+{
+ "lookup" : "6-3",
+"hour" : 6,
+"day": 3
+},
+{
+"lookup" : "7-3",
+"hour" : 7,
+"day": 3 
+},
+{
+"lookup" : "8-3",
+"hour" : 8,
+"day": 3 
+},
+{
+"lookup" : "9-3",
+"hour" : 9,
+"day": 3 
+},
+{
+"lookup" : "10-3",
+"hour" : 10,
+"day": 3 
+},
+{
+"lookup" : "11-3",
+"hour" : 11,
+"day": 3 
+},
+{
+"lookup" : "12-3",
+"hour" : 12,
+"day": 3 
+},
+{
+"lookup" : "13-3",
+"hour" : 13,
+"day": 3 
+},
+{
+"lookup" : "14-3",
+"hour" : 14,
+"day": 3 
+},
+{
+"lookup" : "15-3",
+"hour" : 15,
+"day": 3 
+},
+{
+"lookup" : "16-3",
+"hour" : 16,
+"day": 3 
+},
+{
+"lookup" : "17-3",
+"hour" : 17,
+"day": 3 
+},
+{
+"lookup" : "18-3",
+"hour" : 18,
+"day": 3 
+},
+{
+"lookup" : "19-3",
+"hour" : 19,
+"day": 3 
+},
+{
+"lookup" : "20-3",
+"hour" : 20,
+"day": 3 
+},
+{
+"lookup" : "21-3",
+"hour" : 21,
+"day": 3 
+},
+{
+"lookup" : "22-3",
+"hour" : 22,
+"day": 3 
+},
+{
+"lookup" : "23-3",
+"hour" : 23,
+"day": 3 
+},{
+ "lookup" : "0-4",
+"hour" : 0,
+"day": 4
+},
+{
+"lookup" : "1-4",
+"hour" : 1,
+"day": 4 
+},
+{
+ "lookup" : "2-4",
+"hour" : 2,
+"day": 4
+},
+{
+"lookup" : "3-4",
+"hour" : 3,
+"day": 4 
+},
+{
+ "lookup" : "4-4",
+"hour" : 4,
+"day": 4
+},
+{
+"lookup" : "5-4",
+"hour" : 5,
+"day": 4 
+},
+{
+ "lookup" : "6-4",
+"hour" : 6,
+"day": 4
+},
+{
+"lookup" : "7-4",
+"hour" : 7,
+"day": 4 
+},
+{
+"lookup" : "8-4",
+"hour" : 8,
+"day": 4 
+},
+{
+"lookup" : "9-4",
+"hour" : 9,
+"day": 4 
+},
+{
+"lookup" : "10-4",
+"hour" : 10,
+"day": 4 
+},
+{
+"lookup" : "11-4",
+"hour" : 11,
+"day": 4 
+},
+{
+"lookup" : "12-4",
+"hour" : 12,
+"day": 4 
+},
+{
+"lookup" : "13-4",
+"hour" : 13,
+"day": 4 
+},
+{
+"lookup" : "14-4",
+"hour" : 14,
+"day": 4 
+},
+{
+"lookup" : "15-4",
+"hour" : 15,
+"day": 4 
+},
+{
+"lookup" : "16-4",
+"hour" : 16,
+"day": 4 
+},
+{
+"lookup" : "17-4",
+"hour" : 17,
+"day": 4 
+},
+{
+"lookup" : "18-4",
+"hour" : 18,
+"day": 4 
+},
+{
+"lookup" : "19-4",
+"hour" : 19,
+"day": 4 
+},
+{
+"lookup" : "20-4",
+"hour" : 20,
+"day": 4 
+},
+{
+"lookup" : "21-4",
+"hour" : 21,
+"day": 4 
+},
+{
+"lookup" : "22-4",
+"hour" : 22,
+"day": 4 
+},
+{
+"lookup" : "23-4",
+"hour" : 23,
+"day": 4 
+},{
+ "lookup" : "0-5",
+"hour" : 0,
+"day": 5
+},
+{
+"lookup" : "1-5",
+"hour" : 1,
+"day": 5 
+},
+{
+ "lookup" : "2-5",
+"hour" : 2,
+"day": 5
+},
+{
+"lookup" : "3-5",
+"hour" : 3,
+"day": 5 
+},
+{
+ "lookup" : "4-5",
+"hour" : 4,
+"day": 5
+},
+{
+"lookup" : "5-5",
+"hour" : 5,
+"day": 5 
+},
+{
+ "lookup" : "6-5",
+"hour" : 6,
+"day": 5
+},
+{
+"lookup" : "7-5",
+"hour" : 7,
+"day": 5 
+},
+{
+"lookup" : "8-5",
+"hour" : 8,
+"day": 5 
+},
+{
+"lookup" : "9-5",
+"hour" : 9,
+"day": 5 
+},
+{
+"lookup" : "10-5",
+"hour" : 10,
+"day": 5 
+},
+{
+"lookup" : "11-5",
+"hour" : 11,
+"day": 5 
+},
+{
+"lookup" : "12-5",
+"hour" : 12,
+"day": 5 
+},
+{
+"lookup" : "13-5",
+"hour" : 13,
+"day": 5 
+},
+{
+"lookup" : "14-5",
+"hour" : 14,
+"day": 5 
+},
+{
+"lookup" : "15-5",
+"hour" : 15,
+"day": 5 
+},
+{
+"lookup" : "16-5",
+"hour" : 16,
+"day": 5 
+},
+{
+"lookup" : "17-5",
+"hour" : 17,
+"day": 5 
+},
+{
+"lookup" : "18-5",
+"hour" : 18,
+"day": 5 
+},
+{
+"lookup" : "19-5",
+"hour" : 19,
+"day": 5 
+},
+{
+"lookup" : "20-5",
+"hour" : 20,
+"day": 5 
+},
+{
+"lookup" : "21-5",
+"hour" : 21,
+"day": 5 
+},
+{
+"lookup" : "22-5",
+"hour" : 22,
+"day": 5 
+},
+{
+"lookup" : "23-5",
+"hour" : 23,
+"day": 5 
+},
+{
+ "lookup" : "0-6",
+"hour" : 0,
+"day": 6
+},
+{
+"lookup" : "1-6",
+"hour" : 1,
+"day": 6 
+},
+{
+ "lookup" : "2-6",
+"hour" : 2,
+"day": 6
+},
+{
+"lookup" : "3-6",
+"hour" : 3,
+"day": 6 
+},
+{
+ "lookup" : "4-6",
+"hour" : 4,
+"day": 6
+},
+{
+"lookup" : "5-6",
+"hour" : 5,
+"day": 6 
+},
+{
+ "lookup" : "6-6",
+"hour" : 6,
+"day": 6
+},
+{
+"lookup" : "7-6",
+"hour" : 7,
+"day": 6 
+},
+{
+"lookup" : "8-6",
+"hour" : 8,
+"day": 6 
+},
+{
+"lookup" : "9-6",
+"hour" : 9,
+"day": 6 
+},
+{
+"lookup" : "10-6",
+"hour" : 10,
+"day": 6 
+},
+{
+"lookup" : "11-6",
+"hour" : 11,
+"day": 6 
+},
+{
+"lookup" : "12-6",
+"hour" : 12,
+"day": 6 
+},
+{
+"lookup" : "13-6",
+"hour" : 13,
+"day": 6 
+},
+{
+"lookup" : "14-6",
+"hour" : 14,
+"day": 6 
+},
+{
+"lookup" : "15-6",
+"hour" : 15,
+"day": 6 
+},
+{
+"lookup" : "16-6",
+"hour" : 16,
+"day": 6 
+},
+{
+"lookup" : "17-6",
+"hour" : 17,
+"day": 6 
+},
+{
+"lookup" : "18-6",
+"hour" : 18,
+"day": 6 
+},
+{
+"lookup" : "19-6",
+"hour" : 19,
+"day": 6 
+},
+{
+"lookup" : "20-6",
+"hour" : 20,
+"day": 6 
+},
+{
+"lookup" : "21-6",
+"hour" : 21,
+"day": 6 
+},
+{
+"lookup" : "22-6",
+"hour" : 22,
+"day": 6 
+},
+{
+"lookup" : "23-6",
+"hour" : 23,
+"day": 6 
+}
+]
+
+
+       var margin = { top: 50, right: 0, bottom: 100, left: 100 },
+          width = 760 - margin.left - margin.right,
+          height = 300 - margin.top - margin.bottom,
+          gridSize = Math.floor(width / 24),
+          gridSizeW= Math.floor(width / 24),
+          gridSizeH= Math.floor(height /7),
+          legendElementWidth = gridSize*2,
+          buckets = 9,
+          //colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
+        //  colors = ["#c0c8bd","#b2baad","#a2ad9d","#929d8c","#808e77","#826851","#934231","#ac2718","#cf0e07"],
+          colors = ["#ffffe5","#f7fcb9","#d9f0a3","#addd8e","#78c679","#41ab5d","#238443","#005a32"],
+ 
+          days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ],
+         // times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"]
+           times = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"]
+        // times = ["01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"]
+           console.log(combinations);
+          var array1 = []
+          d3.json("data/output_limit_30.json", function(data){
+            i = 0
+
+            for(c in combinations){
+              checkin_lookup = combinations[c].lookup
+              if(data[i].checkin.checkin_info[checkin_lookup]){
+                var info = { day: combinations[c].day, hour:combinations[c].hour, value: data[i].checkin.checkin_info[checkin_lookup]}
+                array1.push(info)
+              }else{
+                var info = { day: combinations[c].day, hour:combinations[c].hour, value: 0}
+                array1.push(info)
+              }
+            }
+            data = array1
+           
+
+          var colorScale = d3.scale.quantile()
+              .domain([0, buckets - 1, d3.max(data, function (d) { return d.value; })])
+              .range(colors);
+
+          var svg = d3.select("#chart").append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+          var dayLabels = svg.selectAll(".dayLabel")
+              .data(days)
+              .enter().append("text")
+                .text(function (d) { return d; })
+                .attr("x", 0)
+                .attr("y", function (d, i) { return i * gridSizeH; })
+                .style("text-anchor", "end")
+              //  .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
+              .attr("transform", "translate(-6," + gridSizeH / 1.5 + ")")
+              //  .attr("class", function (d, i) { return ((i >= 1 && i <= 5) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+               .attr("class", function (d, i) { return ((i >= 0 && i <= 6) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+
+          var timeLabels = svg.selectAll(".timeLabel")
+              .data(times)
+              .enter().append("text")
+                .text(function(d) { return d; })
+                .attr("x", function(d, i) { return i * gridSize; })
+                .attr("y", 0)
+                .style("text-anchor", "middle")
+                .attr("transform", "translate(" + gridSize / 2 + ", -6)")
+                .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
+
+          var heatMap = svg.selectAll(".hour")
+              .data(data)
+              .enter().append("rect")
+              .attr("x", function(d) { return (d.hour  ) * gridSizeW; })
+              .attr("y", function(d) { return (d.day ) * gridSizeH; })
+              // .attr("rx", 2)
+              // .attr("ry", 2)
+              // .attr("class", "hour bordered")
+              .attr("class", "hour")
+              .attr("width", gridSizeW)
+              .attr("height", gridSizeH)
+              .style("fill", colors[0]) 
+            //   .style("fill", function(d) {  
+            //   if (d.hour<4 || d.hour>20 )  {
+            //     return "#f5f5f5" 
+            // } else {
+            //     return colors[0]
+            // }
+
+            //  })
+              .style("stroke", "#ececec") 
+               //.style("stroke", "white") 
+              .style("stroke-width", 0.2);
+
+          heatMap.transition().duration(1000)
+              //.style("fill", function(d) { return colorScale(d.value); });
+            .style("fill", function(d) { 
+             if (d.hour<4 || d.hour>20 )  {
+                return "#fcfcfc" 
+                return "#f3fcf0"  
+            } else {
+               return colorScale(d.value*1)
+            }  
+          });
+ 
+          heatMap.append("title").text(function(d) { return d.value; });
+              
+          var legend = svg.selectAll(".legend")
+              .data([0].concat(colorScale.quantiles()), function(d) { return d; })
+              .enter().append("g")
+              .attr("class", "legend");
+
+          legend.append("rect")
+            .attr("x", function(d, i) { return legendElementWidth * i; })
+            .attr("y", height)
+            .attr("width", legendElementWidth)
+            .attr("height", gridSize / 2)
+            .style("fill", function(d, i) { return colors[i]; });
+
+          legend.append("text")
+            .attr("class", "mono")
+            //.text(function(d) { return "≥ " + Math.round(d); })
+            .text(function(d) { return  Math.round(d); })
+            .attr("x", function(d, i) { return legendElementWidth * i+5; })
+            .attr("y", height+13);
+      
+
+           var gridSize2=14;
+
+           
+            
+      });
